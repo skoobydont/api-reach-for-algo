75a2c77bf3946c1822cb4ac08b62fc03b14c1909
skoobydont <31373232+skoobydont@users.noreply.github.com>
2022-03-01 00:17:27 -0500

    comment out unhelpful code. would be nice to figure out how to access the application properties file values

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
+		// System.out.println("Starting on port: " + System.getenv("server-port"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
